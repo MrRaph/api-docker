7f099e6eff1f75fa6992f038075b181d85ecedb0
MrRaph_ <mrraph_@techan.fr>
2017-01-15 20:15:03 +0100

    ;)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=app.py
 
-sed -i.bak '/search cloud.online.net/d' /etc/resolv.conf
+# sed -i.bak '/search cloud.online.net/d' /etc/resolv.conf
 
 python -m flask run --host=0.0.0.0
