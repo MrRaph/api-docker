2a0d8972b781148ab956a611ca6a007e48ff5d6e
MrRaph_ <mrraph_@techan.fr>
2017-01-17 13:57:01 +0100

    .

--- api-to-mongo/app.py
+++ api-to-mongo/app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/api?replicaSet=techan'
 mongo = PyMongo(app, config_prefix='MONGO')
 
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=['GET', 'POST'])
 def user_profile(username):
     # user = mongo.db.users.find_one_or_404({'_id': username})
+    # print(request.method)
     if request.method == 'GET':
         user = mongo.db.api.find_one_or_404({'_id': username})
     elif request.method == 'POST':
@@ -17,6 +18,7 @@ def user_profile(username):
         data = request.data
         dataDict = json.loads(data)
         print(dataDict)
+        return dataDict
     # return render_template('user.html',
     #     user=user)
     # return {
@@ -25,7 +27,8 @@ def user_profile(username):
     #     'tasks': tasks,
     #     'error': ''
     # }
-        return "GET"
+    return "Toto"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
