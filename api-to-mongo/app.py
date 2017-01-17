28b5543aa2b33761885efe597689562a8cc6cb01
MrRaph_ <mrraph_@techan.fr>
2017-01-17 10:36:29 +0100

    Erreur quand tu nous tiens ...

--- api-to-mongo/app.py
+++ api-to-mongo/app.py
@@ -1,7 +1,7 @@
 from flask import request, url_for
 from flask.ext.pymongo import PyMongo
 
-app = FlaskAPI(__name__)
+app = Flask(__name__)
 # connect to another MongoDB server altogether
 app.config['MONGO_URI'] = 'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/api?replicaSet=techan'
 mongo = PyMongo(app, config_prefix='MONGO')
