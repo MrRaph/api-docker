0d5d0a00ee7394221c72e3456665ef82e13f6686
MrRaph_ <mrraph_@techan.fr>
2017-01-17 15:18:27 +0100

    .

--- api-to-mongo/app.py
+++ api-to-mongo/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, url_for
 from flask_pymongo import PyMongo
 import json
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 # connect to another MongoDB server altogether
 app.config['MONGO_URI'] = 'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/api?replicaSet=techan'
 mongo = PyMongo(app, config_prefix='MONGO')
